c77433f474f132194f664316a986ce65e55a6bb9
Eduardo Andrade <eduandrade1979@outlook.com>
2025-02-21 12:42:44 -0300

    selectorsList object added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,26 @@
 describe('Swag Labs tests', () => {
+  const selectorsList = {
+    usernameField: "[name='user-name']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionSecondaryHeader: "[data-test='title']",
+    wrogCredentialAlert: "[data-test='error']",
+  };
+
   it('login - success', () => {
     cy.visit('https://www.saucedemo.com/');
-    cy.get("[name='user-name']").type('standard_user');
-    cy.get("[name='password']").type('secret_sauce');
-    cy.get("[type='submit']").click();
+    cy.get(selectorsList.usernameField).type('standard_user');
+    cy.get(selectorsList.passwordField).type('secret_sauce');
+    cy.get(selectorsList.loginButton).click();
     cy.location('pathname').should('equals', '/inventory.html');
-    cy.get('[data-test="title"]').contains('Products');
+    cy.get(selectorsList.sectionSecondaryHeader).contains('Products');
   });
 
   it('login - fail', () => {
     cy.visit('https://www.saucedemo.com/');
-    cy.get("[name='user-name']").type('test');
-    cy.get("[name='password']").type('test');
-    cy.get("[type='submit']").click();
-    cy.get('[data-test="error"]');
+    cy.get(selectorsList.usernameField).type('test');
+    cy.get(selectorsList.passwordField).type('test');
+    cy.get(selectorsList.loginButton).click();
+    cy.get(selectorsList.wrogCredentialAlert);
   });
 });
