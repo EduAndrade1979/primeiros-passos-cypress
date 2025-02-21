c8ce7a5f59877e49d3cb7a6c8b0496bb5f66ed14
Eduardo Andrade <eduandrade1979@outlook.com>
2025-02-21 14:13:13 -0300

    Creating and applying the  userSuccess and userFail objects

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -5,21 +5,32 @@ describe('Swag Labs tests', () => {
     loginButton: "[type='submit']",
     sectionSecondaryHeader: "[data-test='title']",
     wrogCredentialAlert: "[data-test='error']",
+    inventoryList: '.inventory_list',
+  };
+
+  const userSuccess = {
+    name: 'standard_user',
+    password: 'secret_sauce',
+  };
+
+  const userFail = {
+    name: 'test',
+    password: 'test',
   };
 
   it('login - success', () => {
     cy.visit('https://www.saucedemo.com/');
-    cy.get(selectorsList.usernameField).type('standard_user');
-    cy.get(selectorsList.passwordField).type('secret_sauce');
+    cy.get(selectorsList.usernameField).type(userSuccess.name);
+    cy.get(selectorsList.passwordField).type(userSuccess.password);
     cy.get(selectorsList.loginButton).click();
     cy.location('pathname').should('equals', '/inventory.html');
-    cy.get(selectorsList.sectionSecondaryHeader).contains('Products');
+    cy.get(selectorsList.inventoryList);
   });
 
   it('login - fail', () => {
     cy.visit('https://www.saucedemo.com/');
-    cy.get(selectorsList.usernameField).type('test');
-    cy.get(selectorsList.passwordField).type('test');
+    cy.get(selectorsList.usernameField).type(userFail.name);
+    cy.get(selectorsList.passwordField).type(userFail.password);
     cy.get(selectorsList.loginButton).click();
     cy.get(selectorsList.wrogCredentialAlert);
   });
